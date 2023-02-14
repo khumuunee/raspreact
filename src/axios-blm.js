c6dd485a5fdc80bc3b8b6f402151b199de9de709
khumuunee <76699571+khumuunee@users.noreply.github.com>
2023-02-14 22:02:13 +0800

    change port

--- src/axios-blm.js
+++ src/axios-blm.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 console.log('host: ', window.location.host.split(':')[0])
 
 const axiosInstance = axios.create({
-  baseURL: 'http://' + window.location.host.split(':')[0] + ':8080',
+  baseURL: 'http://' + window.location.host.split(':')[0] + ':8181',
 })
 
 export default axiosInstance
